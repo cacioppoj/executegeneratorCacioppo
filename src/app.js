b52730a83574e3232b895caa5896cf0f417cf204
Jose Daniel Cacioppo Armas <danielcacioppo@gmail.com>
2022-08-22 23:08:22 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+const random = number => Math.floor(Math.random() * number);
+
+const generateExcuse = (listWho, listWhat, listWhen) => {
+  return (
+    listWho[random(listWho.length)] +
+    " " +
+    listWhat[random(listWhat.length)] +
+    " " +
+    listWhen[random(listWhen.length)]
+  );
+};
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = document.getElementById("excuse");
+  excuse.innerHTML = generateExcuse(who, what, when);
 };
